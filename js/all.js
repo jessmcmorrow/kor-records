10594f7a94c44d69811d0e1ea2665ba04b8ff3c8
rob alexander <itsticksco@gmail.com>
2021-12-14 20:36:26 +0000

    slider autoplay pauses/unpauses on click+touch

--- js/all.js
+++ js/all.js
@@ -1,100 +1,88 @@
-function logoRotate(){
-    window.onscroll = function(){
+function logoRotate() {
+    window.onscroll = function() {
         var rotate = Math.round(360 * (window.scrollY / (document.body.scrollHeight - window.innerHeight)));
-        document.getElementById('logo').style.transform = "rotate("+rotate+"deg)";;
+        document.getElementById('logo').style.transform = "rotate(" + rotate + "deg)";
+        ;
     }
 }
-    
-    function slider(){
+
+function slider() {
 
     var slider = document.getElementById('slider')
     var sliderItems = [].slice.call(slider.getElementsByClassName('banner-container'));
     var dotsContainer = document.getElementById('dots');
-    
-    
-    sliderItems.forEach(function(item,i){
+
+    sliderItems.forEach(function(item, i) {
         var dot = document.createElement('li');
         dotsContainer.appendChild(dot);
         dot.classList.add('list-inline-item');
         dot.setAttribute("id", i);
         dot.innerHTML = i;
-        dot.addEventListener("click", runSlider);
-        if(i==0){
+        dot.onclick = runSlider;
+        if (i == 0) {
             dot.classList.add('active')
         }
     })
 
     var dots = [].slice.call(dotsContainer.getElementsByTagName('li'));
 
-    
     function runSlider() {
         var dnum = this.getAttribute("id");
-        
-    sliderItems.forEach(function(item,i){
+        sliderItems.forEach(function(item, i) {
             item.classList.remove('active');
             dots[i].classList.remove('active');
-
             sliderItems[dnum].classList.add('active');
             dots[dnum].classList.add('active');
-    });
-        
+        });
+
     }
 
-    function goToNextSlide(){
+    function goToNextSlide() {
         var lastSlideId = (dots.length - 1).toString();
-                
-        var currentSlideId = dots.filter(x=>x.className.indexOf('active')!=-1)[0].id;
-        
-        var nextSlideId = currentSlideId !== lastSlideId ? (parseInt(currentSlideId)+1).toString() : "0";
-    
+
+        var currentSlideId = dots.filter(function(x) {
+            return x.className.indexOf('active') != -1
+        })[0].id;
+
+        var nextSlideId = currentSlideId !== lastSlideId ? (parseInt(currentSlideId) + 1).toString() : "0";
+
         document.getElementById(nextSlideId).click();
     }
-    
-    
-    function autoSlide(){
+
+    function autoSlide() {
 
         var secondsPerSlide = 4;
-        var framesPerSlide = secondsPerSlide*60;
+        var framesPerSlide = secondsPerSlide * 60;
         var c = 0;
         var paused = false;
 
-var slideLoader = document.getElementById('slide-loader');
+        var slideLoader = document.getElementById('slide-loader');
 
-function animation() {
-    c++;
-    if(c % (framesPerSlide) == 0 && !paused){
-        goToNextSlide();
-}
-if(!paused){
-    slideLoader.style.width = ((c % (framesPerSlide)) / (framesPerSlide))*100 + "%";
-}
-window.requestAnimationFrame(animation);
+        function animation() {
+            c++;
+            if (c % (framesPerSlide) == 0 && !paused) {
+                goToNextSlide();
+            }
+            if (!paused) {
+                slideLoader.style.width = ((c % (framesPerSlide)) / (framesPerSlide)) * 100 + "%";
+            }
+            window.requestAnimationFrame(animation);
 
-}
+        }
 
-window.requestAnimationFrame(animation);
+        window.requestAnimationFrame(animation);
 
-
-
-        // slider.onmousein = function(){
-        //     paused = true;
-        // }
-
-        // slider.onmouseout = function(){
-        //     paused = false;
-        // }
-
-        // slider.onclick = function(){
-        //     paused = !paused;
-        // }
+        sliderItems.forEach(function(item) {
+            item.onclick = function() {
+                paused = !paused;
+            }
+        });
 
     }
 
     autoSlide();
 
-    }
-    
-    
+}
 
 logoRotate();
 slider();
